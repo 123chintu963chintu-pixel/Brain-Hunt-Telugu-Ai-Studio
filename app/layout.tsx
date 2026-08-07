651c02c8673ccb006f6c033f43d12297fdef2329
123chintu963chintu-pixel <123chintu963chintu@gmail.com>
2026-08-08 00:05:37 +0530

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Brain Hunt Telugu AI Studio",
+  description: "AI Studio",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
